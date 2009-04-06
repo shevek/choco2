37b8cc5cf0aad338545bea1491d53bb57204c91d
scand1sk <scand1sk@5779b9a9-2447-0410-887e-bb98d57c6de4>
2009-04-06 10:05:57 +0000

    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1016 5779b9a9-2447-0410-887e-bb98d57c6de4

--- choco-contribs/src/main/java/samples/random/RandomProblem.java
+++ choco-contribs/src/main/java/samples/random/RandomProblem.java
@@ -48,382 +48,112 @@ import java.util.*;
 
 public class RandomProblem {
 
-	private IntegerVariable[] variables;
-	private Collection<Constraint> constraints;
+    private IntegerVariable[] variables;
+    private Collection<Constraint> constraints;
 
-	public RandomProblem(int nbVar, int nbVal, double density, long seed,
-			boolean force) {
-		this(nbVar, nbVal, nbArcs(nbVar, density), criticTightness(nbVar,
-				nbVal, nbArcs(nbVar, density)), seed, force);
-	}
+    public RandomProblem(int nbVar, int nbVal, double density, long seed,
+            boolean force) {
+        this(nbVar, nbVal, nbArcs(nbVar, density), criticTightness(nbVar,
+                nbVal, nbArcs(nbVar, density)), seed, force);
+    }
 
-	public RandomProblem(int nbVar, int nbVal, int nbConstraints,
-			double tightness, long seed, boolean force) {
-		// System.out.println("Generating problem with " + nbVar +
-		// " variables, "
-		// + nbVal + " values, " + nbConstraints + " constraints, "
-		// + tightness + " Tightness");
+    public RandomProblem(int nbVar, int nbVal, int nbConstraints,
+            double tightness, long seed, boolean force) {
+        // System.out.println("Generating problem with " + nbVar +
+        // " variables, "
+        // + nbVal + " values, " + nbConstraints + " constraints, "
+        // + tightness + " Tightness");
 
-		// Build enumerated domain variables
-		variables = new IntegerVariable[nbVar];
+        // Build enumerated domain variables
+        variables = new IntegerVariable[nbVar];
 
-		for (int i = nbVar; --i >= 0;) {
-			variables[i] = makeIntVar("var" + i, 0, nbVal - 1);
-		}
+        for (int i = nbVar; --i >= 0;) {
+            variables[i] = makeIntVar("var" + i, 0, nbVal - 1);
+        }
 
-		final ProportionRandomListGenerator r = new CoarseProportionRandomListGenerator(
-				nbVar, 2, seed);
+        final ProportionRandomListGenerator r = new CoarseProportionRandomListGenerator(
+                nbVar, 2, seed);
 
-		final int[][] activeConstraints = r.selectTuples(nbConstraints,
-				Structure.UNSTRUCTURED, false, false);
+        final int[][] activeConstraints = r.selectTuples(nbConstraints,
+                Structure.UNSTRUCTURED, false, false);
 
-		final int[] solution;
+        final int[] solution;
 
-		final Random random = new Random(seed);
+        final Random random = new Random(seed);
 
-		if (force) {
-			solution = new int[nbVar];
-			for (int i = nbVar; --i >= 0;) {
-				solution[i] = random.nextInt(nbVal);
-			}
-		} else {
-			solution = null;
-		}
+        if (force) {
+            solution = new int[nbVar];
+            for (int i = nbVar; --i >= 0;) {
+                solution[i] = random.nextInt(nbVal);
+            }
+        } else {
+            solution = null;
+        }
 
-		constraints = new ArrayList<Constraint>();
+        constraints = new ArrayList<Constraint>();
 
-		final int[] sizes = { nbVal, nbVal };
+        final int[] sizes = { nbVal, nbVal };
 
-		for (int[] arc : activeConstraints) {
+        for (int[] arc : activeConstraints) {
 
-			final boolean[][] matrix = new boolean[nbVal][nbVal];
+            final boolean[][] matrix = new boolean[nbVal][nbVal];
 
-			ProportionRandomListGenerator randomList = new CoarseProportionRandomListGenerator(
-					sizes, random.nextLong());
+            ProportionRandomListGenerator randomList = new CoarseProportionRandomListGenerator(
+                    sizes, random.nextLong());
 
-			final int[][] tupleList;
+            final int[][] tupleList;
 
-			if (force) {
-				tupleList = randomList.selectTuples(
-						(int) (nbVal * nbVal * (1 - tightness)),
-						Structure.UNSTRUCTURED, false, true, new int[] {
-								solution[arc[0]], solution[arc[1]] }, true);
-			} else {
-				tupleList = randomList.selectTuples(
-						(int) (nbVal * nbVal * (1 - tightness)),
-						Structure.UNSTRUCTURED, false, true);
-			}
+            if (force) {
+                tupleList = randomList.selectTuples(
+                        (int) (nbVal * nbVal * (1 - tightness)),
+                        Structure.UNSTRUCTURED, false, true, new int[] {
+                                solution[arc[0]], solution[arc[1]] }, true);
+            } else {
+                tupleList = randomList.selectTuples(
+                        (int) (nbVal * nbVal * (1 - tightness)),
+                        Structure.UNSTRUCTURED, false, true);
+            }
 
-			for (int[] tuple : tupleList) {
-				matrix[tuple[0]][tuple[1]] = true;
-			}
+            for (int[] tuple : tupleList) {
+                matrix[tuple[0]][tuple[1]] = true;
+            }
 
-			constraints.add(feasPairAC(variables[arc[0]], variables[arc[1]],
-					matrix));
-		}
-	}
+            constraints.add(feasPairAC("cp:ac32", variables[arc[0]],
+                    variables[arc[1]], matrix));
+        }
+    }
 
-	public IntegerVariable[] getVariables() {
-		return variables;
-	}
+    public static int nbArcs(double n, double density) {
+        return (int) (density * n * (n - 1) / 2d);
+    }
 
-	public Collection<Constraint> getConstraints() {
-		return constraints;
-	}
+    public static double criticTightness(int nbvar, int nbval, int nbcons) {
+        // d = n^alpha
+        double alpha = Math.log(nbval) / Math.log(nbvar);
 
-	private static double criticTightness(int nbvar, int nbval, int nbcons) {
-		// d = n^alpha
-		double alpha = Math.log(nbval) / Math.log(nbvar);
+        if (alpha <= .5) {
+            throw new InvalidParameterException();
+        }
 
-		if (alpha <= .5) {
-			throw new InvalidParameterException();
-		}
+        // e = rn ln(n)
+        double r = nbcons / (nbvar * Math.log(nbvar));
 
-		// e = rn ln(n)
-		double r = nbcons / (nbvar * Math.log(nbvar));
+        // pcr = 1 − exp(−alpha/r)
+        double pcr = 1 - Math.exp(-alpha / r);
 
-		// pcr = 1 − exp(−alpha/r)
-		double pcr = 1 - Math.exp(-alpha / r);
+        // if (pcr > .5) {
+        // System.err.println("Warning : tightness should not be > 0.5");
+        // }
 
-		// if (pcr > .5) {
-		// System.err.println("Warning : tightness should not be > 0.5");
-		// }
+        return pcr;
+    }
 
-		return pcr;
-	}
+    public IntegerVariable[] getVariables() {
+        return variables;
+    }
 
-	private static int nbArcs(double n, double density) {
-		return (int) (density * n * (n - 1) / 2d);
-	}
+    public Collection<Constraint> getConstraints() {
+        return constraints;
+    }
 
-	private static int NBINSTANCES = -1;
-
-	private static int TIMEOUT = -1;
-
-	private enum Heuristic {
-		DDEG, WDEG, PROPAGATE
-	}
-
-	public static void main(String args[]) {
-		final int nbVar;
-		final int nbVal;
-		final double density;
-		final int e;
-		final long seed;
-		final boolean force;
-
-		final double tightness;
-
-		final double maxRPCProp;
-
-		final Heuristic heuristic;
-
-		final boolean light;
-		try {
-			int i = 0;
-			nbVar = Integer.valueOf(args[i++]);
-			nbVal = Integer.valueOf(args[i++]);
-
-			density = Double.valueOf(args[i++]);
-
-			tightness = Double.valueOf(args[i++]);
-
-			force = Boolean.valueOf(args[i++]);
-
-			seed = Long.valueOf(args[i++]);
-
-			heuristic = Heuristic.valueOf(args[i++]);
-
-			maxRPCProp = Double.valueOf(args[i++]);
-
-			light = Boolean.valueOf(args[i++]);
-
-			TIMEOUT = 1000 * Integer.valueOf(args[i++]);
-
-			NBINSTANCES = Integer.valueOf(args[i++]);
-
-		} catch (Exception exception) {
-			System.out
-					.println("Usage : RandomProblem nbVar nbVal density tightness force seed heuristic maxrpcprop light timeout nbinstances");
-			return;
-		}
-
-		e = nbArcs(nbVar, density);
-
-		System.out.println("-------------------");
-		System.out.println(nbVar + " var, " + nbVal + " val, " + density
-				+ " density (" + e + " cstr), " + tightness + " tightness"
-				+ (force ? ", forced" : "") + " (pcr = "
-				+ criticTightness(nbVar, nbVal, e) + "), " + maxRPCProp
-				+ " maxrpc, " + (light ? "light" : "full") + ", " + heuristic);
-
-		test(light, nbVar, nbVal, e, tightness, seed, force, heuristic,
-				maxRPCProp);
-
-	}
-
-	private static Collection<Clique> cliques(List<Constraint> constraints) {
-		final Collection<Clique> cliques = new ArrayList<Clique>();
-
-		for (int ci1 = constraints.size(); --ci1 >= 0;) {
-			final Constraint c1 = constraints.get(ci1);
-			final Set<Variable> c1Scope = new HashSet<Variable>(Arrays
-					.asList(c1.getVariables()));
-			for (int ci2 = ci1; --ci2 >= 0;) {
-				final Constraint c2 = constraints.get(ci2);
-
-				final Variable v3;
-
-				if (c1Scope.contains(c2.getVariables()[0])) {
-					v3 = c2.getVariables()[1];
-				} else if (c1Scope.contains(c2.getVariables()[1])) {
-					v3 = c2.getVariables()[0];
-				} else {
-					continue;
-				}
-
-				for (int ci3 = ci2; --ci3 >= 0;) {
-					final Constraint c3 = constraints.get(ci3);
-					if ((v3 == c3.getVariables()[0] && c1Scope.contains(c3
-							.getVariables()[1]))
-							|| (v3 == c3.getVariables()[1] && c1Scope
-									.contains(c3.getVariables()[0]))) {
-						cliques.add(new Clique(c1, c2, c3));
-						break;
-					}
-				}
-			}
-		}
-		return cliques;
-	}
-
-	private static void test(boolean light, int nbVar, int nbVal, int nbCons,
-			double tightness, long seed, boolean force, Heuristic heuristic,
-			double maxRPCProp) {
-		final List<Integer> nodes = new ArrayList<Integer>(NBINSTANCES);
-		final List<Double> cpu = new ArrayList<Double>(NBINSTANCES);
-		final List<Integer> nbCliques = new ArrayList<Integer>(NBINSTANCES);
-		final List<Integer> nbMaxRPCCons = new ArrayList<Integer>(NBINSTANCES);
-		final List<Integer> nbAwakes = new ArrayList<Integer>(NBINSTANCES);
-		final List<Long> mem = new ArrayList<Long>(NBINSTANCES);
-		// final double[] nps = new double[NBINSTANCES - 1];
-
-		for (int i = NBINSTANCES; --i >= 0;) {
-			System.out.print("g");
-			final RandomProblem problem = new RandomProblem(nbVar, nbVal,
-					nbCons, tightness, seed + i, force);
-
-			final List<Constraint> constraints = new ArrayList<Constraint>(
-					problem.getConstraints());
-
-			// Build a model
-			final Model m = new CPModel();
-
-			m.addVariables(problem.getVariables());
-
-			final Set<Constraint> maxRPCConstraints = new HashSet<Constraint>();
-			final Set<Constraint> acConstraints = new HashSet<Constraint>(
-					constraints);
-
-			if (maxRPCProp > 1) {
-				nbCliques.add(-1);
-				maxRPCConstraints.addAll(acConstraints);
-				acConstraints.clear();
-			} else {
-				final Collection<Clique> cliques = cliques(constraints);
-				nbCliques.add(cliques.size());
-				int nbMaxRPC = (int) (cliques.size() * maxRPCProp);
-				for (Clique c : cliques) {
-					if (nbMaxRPC-- <= 0) {
-						break;
-					}
-					maxRPCConstraints.addAll(Arrays.asList(c.constraints));
-				}
-			}
-
-			if (!maxRPCConstraints.isEmpty()) {
-				final Set<Variable> maxRPCVariables = new HashSet<Variable>();
-				for (Constraint c : maxRPCConstraints) {
-					maxRPCVariables.addAll(Arrays.asList(c.getVariables()));
-				}
-
-				final Constraint cc = new ComponentConstraintWithSubConstraints(
-						StrongConsistencyManager.class, maxRPCVariables
-								.toArray(new Variable[maxRPCVariables.size()]),
-						MaxRPCrm.class, maxRPCConstraints
-								.toArray(new Constraint[maxRPCConstraints
-										.size()]));
-				if (light) {
-					cc.addOption("light");
-				}
-				m.addConstraint(cc);
-				acConstraints.removeAll(maxRPCConstraints);
-			}
-
-			nbMaxRPCCons.add(maxRPCConstraints.size());
-
-			for (Constraint c : acConstraints) {
-				m.addConstraint(c);
-			}
-
-			// System.out.println("Building solver...");
-
-			// Build a solver
-			final Solver s = new CPSolver();
-			// CPSolver.setVerbosity(CPSolver.SEARCH);
-			// Read the model
-			s.read(m);
-
-			s.setTimeLimit(TIMEOUT);
-			// System.out.println("Solving...");
-			System.out.print("s");
-			Boolean result;
-			System.gc();
-
-			long time;
-
-			if (heuristic.equals(Heuristic.PROPAGATE)) {
-				time = -System.nanoTime();
-				try {
-					s.propagate();
-					result = Boolean.TRUE;
-				} catch (ContradictionException e) {
-					result = Boolean.FALSE;
-				}
-				time += System.nanoTime();
-			} else {
-				switch (heuristic) {
-				case WDEG:
-					// s.attachGoal(new DomOverWDegBranching(s, new MinVal()));
-//					s.attachGoal(new AssignOrForbidIntVarVal(
-//							new DomOverWDeg(s), new MinVal()));
-					
-					// s.setVarIntSelector(new DomOverWDegRPC(s));
-					// s
-					// .attachGoal(new
-					// choco.cp.solver.search.integer
-					// .branching.DomOverWDegBranching(
-					// s, new MinVal()));
-					// s
-					// .attachGoal(new AssignVar(new DomOverWDeg(s),
-					// new MinVal()));
-					 s.attachGoal(new DomOverWDegBranching(s, new IncreasingDomain()));
-				default:
-					s.attachGoal(new AssignOrForbidIntVarVal(
-							new DomOverDDegRPC(s), new MinVal()));
-				}
-				result = s.solve();
-				time = (long) s.getTimeCount() * 1000000l;
-			}
-
-			// final Boolean result = s.solve();
-			mem.add(Runtime.getRuntime().totalMemory()
-					- Runtime.getRuntime().freeMemory());
-			// CPSolver.flushLogs();
-
-			if (result == null) {
-				System.out.print("*");
-				// System.out.print(">=" + nbSol);
-				// nodes[i] = Integer.MAX_VALUE;
-				cpu.add(Double.POSITIVE_INFINITY);
-				nbAwakes.add(Integer.MAX_VALUE);
-				nodes.add(Integer.MAX_VALUE);
-			} else {
-				System.out.print(result ? 1 : 0);
-				try {
-					nodes.add(s.getNodeCount());
-				} catch (Exception e) {
-					nodes.add(0);
-				}
-				cpu.add(time / 1e9d);
-				nbAwakes.add(MaxRPCrm.nbPropag);
-				MaxRPCrm.nbPropag = 0;
-			}
-			// nps[i] = 1000 * s.getNodeCount() / s.getTimeCount();
-
-		}
-
-		System.out.println();
-
-		System.out.println(median(cpu) + " seconds med");
-		System.out.println(median(nodes) + " nodes med");
-		System.out.println(median(nbCliques) + " cliques med");
-		System.out.println(median(nbMaxRPCCons) + " maxrpccons med");
-		System.out.println(median(nbAwakes) + " awakes med");
-		System.out.println(median(mem) + " mem med");
-	}
-
-	private static class Clique {
-		final Constraint[] constraints;
-
-		public Clique(Constraint c1, Constraint c2, Constraint c3) {
-			constraints = new Constraint[] { c1, c2, c3 };
-		}
-	}
-
-	private static <T extends Comparable<T>> T median(List<T> array) {
-		final List<T> sorted = new ArrayList<T>(array);
-		Collections.sort(sorted);
-		return sorted.get((sorted.size() - 1) / 2);
-	}
 }
---
+++ choco-contribs/src/main/java/samples/random/RandomProblemLauncher.java
@@ -0,0 +1,341 @@
+package samples.random;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import choco.cp.model.CPModel;
+import choco.cp.solver.CPSolver;
+import choco.cp.solver.constraints.strong.DomOverDDegRPC;
+import choco.cp.solver.constraints.strong.DomOverWDegRPC;
+import choco.cp.solver.constraints.strong.StrongConsistencyManager;
+import choco.cp.solver.constraints.strong.maxrpcrm.MaxRPCrm;
+import choco.cp.solver.search.integer.branching.AssignOrForbidIntVarVal;
+import choco.cp.solver.search.integer.valselector.MinVal;
+import choco.kernel.model.Model;
+import choco.kernel.model.constraints.ComponentConstraintWithSubConstraints;
+import choco.kernel.model.constraints.Constraint;
+import choco.kernel.model.variables.Variable;
+import choco.kernel.solver.ContradictionException;
+import choco.kernel.solver.Solver;
+
+public class RandomProblemLauncher {
+
+    private static int NBINSTANCES = -1;
+
+    private static int TIMEOUT = -1;
+
+    private enum Heuristic {
+        DDEG, WDEG, PROPAGATE
+    }
+
+    private enum Filter {
+        AC, MaxRPCLight, MaxRPC;
+
+        public String toString() {
+            switch (this) {
+            case AC:
+                return "a";
+            case MaxRPCLight:
+                return "l";
+            case MaxRPC:
+                return "m";
+
+            default:
+                throw new IllegalStateException();
+            }
+        }
+    }
+
+    private RandomProblemLauncher() {
+
+    }
+
+    public static void main(String args[]) {
+        final int nbVar;
+        final int nbVal;
+        final double density;
+        final int e;
+        final long seed;
+        final boolean force;
+
+        final double tightness1;
+        final double tightness2;
+        final double increment;
+
+        final Heuristic heuristic;
+
+        // final Filter filter;
+        try {
+            int i = 0;
+            nbVar = Integer.valueOf(args[i++]);
+            nbVal = Integer.valueOf(args[i++]);
+
+            density = Double.valueOf(args[i++]);
+
+            tightness1 = Double.valueOf(args[i++]);
+            tightness2 = Double.valueOf(args[i++]);
+            increment = Double.valueOf(args[i++]);
+
+            force = Boolean.valueOf(args[i++]);
+
+            seed = Long.valueOf(args[i++]);
+
+            heuristic = Heuristic.valueOf(args[i++]);
+
+            // filter = Filter.valueOf(args[i++]);
+
+            TIMEOUT = 1000 * Integer.valueOf(args[i++]);
+
+            NBINSTANCES = Integer.valueOf(args[i++]);
+
+        } catch (Exception exception) {
+            System.out.println(exception);
+            System.out
+                    .println("Usage : RandomProblem nbVar nbVal density tightness1 tightness2 increment force seed heuristic timeout nbinstances");
+            return;
+        }
+
+        e = RandomProblem.nbArcs(nbVar, density);
+
+        for (double tightness = tightness1; tightness <= tightness2; tightness += increment) {
+            System.out.println("-------------------");
+            System.out.println(nbVar + " var, " + nbVal + " val, " + density
+                    + " density (" + e + " cstr), " + tightness + " tightness"
+                    + (force ? ", forced" : "") + " (pcr = "
+                    + RandomProblem.criticTightness(nbVar, nbVal, e) + "), "
+                    + heuristic);
+
+            test(nbVar, nbVal, e, tightness, seed, force, heuristic);
+        }
+    }
+
+    private static void test(int nbVar, int nbVal, int nbCons,
+            double tightness, long seed, boolean force, Heuristic heuristic) {
+        final Map<Filter, List<Integer>> nodes = new HashMap<Filter, List<Integer>>(
+                Filter.values().length);
+        final Map<Filter, List<Double>> cpu = new HashMap<Filter, List<Double>>(
+                Filter.values().length);
+        final List<Integer> nbCliques = new ArrayList<Integer>(NBINSTANCES);
+        final Map<Filter, List<Integer>> nbAwakes = new HashMap<Filter, List<Integer>>(
+                Filter.values().length);
+        final Map<Filter, List<Long>> mem = new HashMap<Filter, List<Long>>(
+                Filter.values().length);
+        // final double[] nps = new double[NBINSTANCES - 1];
+
+        for (Filter f : Filter.values()) {
+            nodes.put(f, new ArrayList<Integer>(NBINSTANCES));
+            cpu.put(f, new ArrayList<Double>(NBINSTANCES));
+            nbAwakes.put(f, new ArrayList<Integer>(NBINSTANCES));
+            mem.put(f, new ArrayList<Long>(NBINSTANCES));
+        }
+
+        for (int i = NBINSTANCES; --i >= 0;) {
+            System.out.print("g");
+            final RandomProblem problem = new RandomProblem(nbVar, nbVal,
+                    nbCons, tightness, seed + i, force);
+
+            final List<Constraint> constraints = new ArrayList<Constraint>(
+                    problem.getConstraints());
+
+            nbCliques.add(cliques(constraints).size());
+
+            for (Filter filter : Filter.values()) {
+                System.out.print(filter);
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                System.gc();
+                // Build a model
+                final Model m = new CPModel();
+
+                m.addVariables(problem.getVariables());
+
+                switch (filter) {
+                case MaxRPC:
+                case MaxRPCLight:
+                    final Constraint cc = new ComponentConstraintWithSubConstraints(
+                            StrongConsistencyManager.class, problem
+                                    .getVariables(), MaxRPCrm.class,
+                            constraints.toArray(new Constraint[constraints
+                                    .size()]));
+                    if (filter == Filter.MaxRPCLight) {
+                        cc.addOption("light");
+                    }
+                    m.addConstraint(cc);
+                    break;
+                default:
+                    for (Constraint c : constraints) {
+                        m.addConstraint(c);
+                    }
+                }
+
+                // System.out.println("Building solver...");
+
+                // Build a solver
+                final Solver s = new CPSolver();
+                // CPSolver.setVerbosity(CPSolver.SEARCH);
+                // Read the model
+                s.read(m);
+                // System.out.print(s.pretty());
+                s.setTimeLimit(TIMEOUT);
+                // System.out.println("Solving...");
+                Boolean result;
+
+                long time;
+
+                if (heuristic.equals(Heuristic.PROPAGATE)) {
+                    time = -System.nanoTime();
+                    try {
+                        s.propagate();
+                        result = Boolean.TRUE;
+                    } catch (ContradictionException e) {
+                        result = Boolean.FALSE;
+                    }
+                    time += System.nanoTime();
+                } else {
+                    switch (heuristic) {
+                    case WDEG:
+                        // s.attachGoal(new DomOverWDegBranching(s, new
+                        // MinVal()));
+                        s.attachGoal(new AssignOrForbidIntVarVal(
+                                new DomOverWDegRPC(s), new MinVal()));
+
+                        // s.setVarIntSelector(new DomOverWDegRPC(s));
+                        // s
+                        // .attachGoal(new
+                        // choco.cp.solver.search.integer
+                        // .branching.DomOverWDegBranching(
+                        // s, new MinVal()));
+                        // s
+                        // .attachGoal(new AssignVar(new DomOverWDeg(s),
+                        // new MinVal()));
+                        // s.attachGoal(new DomOverWDegBranching(s,
+                        // new IncreasingDomain()));
+                    default:
+                        s.attachGoal(new AssignOrForbidIntVarVal(
+                                new DomOverDDegRPC(s), new MinVal()));
+                    }
+
+                    result = s.solve();
+                    time = (long) s.getTimeCount() * 1000000l;
+                }
+
+                // final Boolean result = s.solve();
+
+                mem.get(filter).add(
+                        Runtime.getRuntime().totalMemory()
+                                - Runtime.getRuntime().freeMemory());
+                // CPSolver.flushLogs();
+
+                if (result == null) {
+                    System.out.print("*");
+                    // System.out.print(">=" + nbSol);
+                    // nodes[i] = Integer.MAX_VALUE;
+                    cpu.get(filter).add(Double.POSITIVE_INFINITY);
+                    nbAwakes.get(filter).add(Integer.MAX_VALUE);
+                    nodes.get(filter).add(Integer.MAX_VALUE);
+                } else {
+                    System.out.print(result ? 1 : 0);
+                    try {
+                        nodes.get(filter).add(s.getNodeCount());
+                    } catch (Exception e) {
+                        nodes.get(filter).add(0);
+                    }
+                    cpu.get(filter).add(time / 1e9d);
+                    nbAwakes.get(filter).add(MaxRPCrm.nbPropag);
+                    MaxRPCrm.nbPropag = 0;
+                }
+                // nps[i] = 1000 * s.getNodeCount() / s.getTimeCount();
+            }
+        }
+
+        System.out.println();
+        System.out.println(median(nbCliques) + " cliques med");
+        for (Filter f : Filter.values()) {
+            System.out.println();
+            System.out.println(f + " :");
+            System.out.println(median(cpu.get(f)) + " seconds med");
+            System.out.println(median(nodes.get(f)) + " nodes med");
+            System.out.println(median(nbAwakes.get(f)) + " awakes med");
+            System.out.println(median(mem.get(f)) + " mem med");
+        }
+    }
+
+    private static class Clique {
+        final Constraint[] constraints;
+
+        public Clique(Constraint c1, Constraint c2, Constraint c3) {
+            constraints = new Constraint[] { c1, c2, c3 };
+        }
+    }
+
+    private static <T extends Comparable<T>> T median(List<T> array) {
+        final List<T> sorted = new ArrayList<T>(array);
+        Collections.sort(sorted);
+        return sorted.get((sorted.size() - 1) / 2);
+    }
+
+    private static Collection<Clique> cliques(List<Constraint> constraints) {
+        final Collection<Clique> cliques = new ArrayList<Clique>();
+
+        for (int ci1 = constraints.size(); --ci1 >= 0;) {
+            final Constraint c1 = constraints.get(ci1);
+            final Set<Variable> c1Scope = new HashSet<Variable>(Arrays
+                    .asList(c1.getVariables()));
+            for (int ci2 = ci1; --ci2 >= 0;) {
+                final Constraint c2 = constraints.get(ci2);
+
+                final Variable v3;
+
+                if (c1Scope.contains(c2.getVariables()[0])) {
+                    v3 = c2.getVariables()[1];
+                } else if (c1Scope.contains(c2.getVariables()[1])) {
+                    v3 = c2.getVariables()[0];
+                } else {
+                    continue;
+                }
+
+                for (int ci3 = ci2; --ci3 >= 0;) {
+                    final Constraint c3 = constraints.get(ci3);
+                    if ((v3 == c3.getVariables()[0] && c1Scope.contains(c3
+                            .getVariables()[1]))
+                            || (v3 == c3.getVariables()[1] && c1Scope
+                                    .contains(c3.getVariables()[0]))) {
+                        cliques.add(new Clique(c1, c2, c3));
+                        break;
+                    }
+                }
+            }
+        }
+        return cliques;
+    }
+}
