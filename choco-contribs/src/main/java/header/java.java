c6bdf0f87e3bdb7e4daf808b45b2e945c795a4d6
cprudhom <cprudhom@5779b9a9-2447-0410-887e-bb98d57c6de4>
2009-07-01 13:35:12 +0000

    - declare files for maven website automatic generation // to complete
    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1112 5779b9a9-2447-0410-887e-bb98d57c6de4

---
+++ choco-contribs/src/main/java/header/java.java
@@ -0,0 +1,35 @@
+/* ************************************************
+ *           _       _                            *
+ *          |  °(..)  |                           *
+ *          |_  J||L _|        CHOCO solver       *
+ *                                                *
+ *     Choco is a java library for constraint     *
+ *     satisfaction problems (CSP), constraint    *
+ *     programming (CP) and explanation-based     *
+ *     constraint solving (e-CP). It is built     *
+ *     on a event-based propagation mechanism     *
+ *     with backtrackable structures.             *
+ *                                                *
+ *     Choco is an open-source software,          *
+ *     distributed under a BSD licence            *
+ *     and hosted by sourceforge.net              *
+ *                                                *
+ *     + website : http://choco.emn.fr            *
+ *     + support : choco@emn.fr                   *
+ *                                                *
+ *     Copyright (C) F. Laburthe,                 *
+ *                   N. Jussien    1999-2009      *
+ **************************************************/
+package header;
+
+// imports...
+
+/*
+* User : Your NAME
+* Mail : your(a)mail.com
+* Date : 26 juin 2009
+* Since : Choco X.Y.Z
+* Update : Choco X.Y.Z
+*/
+public class java {
+}
