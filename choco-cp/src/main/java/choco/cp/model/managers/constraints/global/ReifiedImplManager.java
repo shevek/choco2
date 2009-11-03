51786e1efec1c1dc2d5511070f3816f10fc32ed5
cprudhom <cprudhom@5779b9a9-2447-0410-887e-bb98d57c6de4>
2009-11-03 08:08:32 +0000

    add missing manager
    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1290 5779b9a9-2447-0410-887e-bb98d57c6de4

---
+++ choco-cp/src/main/java/choco/cp/model/managers/constraints/global/ReifiedImplManager.java
@@ -0,0 +1,65 @@
+/* ************************************************
+*           _       _                            *
+*          |  °(..)  |                           *
+*          |_  J||L _|        CHOCO solver       *
+*                                                *
+*     Choco is a java library for constraint     *
+*     satisfaction problems (CSP), constraint    *
+*     programming (CP) and explanation-based     *
+*     constraint solving (e-CP). It is built     *
+*     on a event-based propagation mechanism     *
+*     with backtrackable structures.             *
+*                                                *
+*     Choco is an open-source software,          *
+*     distributed under a BSD licence            *
+*     and hosted by sourceforge.net              *
+*                                                *
+*     + website : http://choco.emn.fr            *
+*     + support : choco@emn.fr                   *
+*                                                *
+*     Copyright (C) F. Laburthe,                 *
+*                   N. Jussien    1999-2009      *
+**************************************************/
+package choco.cp.model.managers.constraints.global;
+
+import choco.Choco;
+import choco.cp.model.managers.IntConstraintManager;
+import choco.cp.solver.CPSolver;
+import choco.cp.solver.constraints.integer.channeling.ReifiedBinImplication;
+import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.Solver;
+import choco.kernel.solver.constraints.SConstraint;
+
+import java.util.HashSet;
+
+/*
+* User : charles
+* Mail : cprudhom(a)emn.fr
+* Date : 26 oct. 2009
+* Since : Choco 2.1.1
+* Update : Choco 2.1.1
+*/
+public class ReifiedImplManager extends IntConstraintManager {
+
+    /**
+     * Build a constraint for the given solver and "model variables"
+     *
+     * @param solver associated solver
+     * @param variables reified variable and literals variables
+     * @param parameters : a "hook" to attach any kind of parameters to constraints
+     * @param options options
+     * @return SConstraint
+     */
+    @Override
+    public SConstraint makeConstraint(Solver solver, IntegerVariable[] variables, Object parameters, HashSet<String> options) {
+        if (solver instanceof CPSolver) {
+            if (parameters == null) {
+                return new ReifiedBinImplication(solver.getVar(variables[0]), solver.getVar(variables[1]), solver.getVar(variables[2]));
+            }
+        }
+        if (Choco.DEBUG) {
+            throw new RuntimeException("Could not found implementation for ReifiedImplication");
+        }
+        return null;
+    }
+}
