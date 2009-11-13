39e454ac70f85b97a56c4b85008106744f04ddef
tpetit <tpetit@5779b9a9-2447-0410-887e-bb98d57c6de4>
2009-11-13 16:25:43 +0000

    A global cardinality constraint ordered w.r.t. cardinalities. First commit.
    File : OrderedGcc.java
    TestFile : OrderedGccTest.java
    Manager : OrderedGccManager.java
     
    - Usage: distribute values in a fixed set of variables (representing penalties) => "cost value v+1 is at least as undesirable as cost value v"
     
     - OrderedGcc holds iff :
        nb_occ(minValue) >= minBot
        For each i in 0..maxValue sum_nb_occ(v s.t. v>=i) <= Imax[i]
      
     - Could be reformulated by :
     
     globalCardinalityVars(vars,cards)
          vith D(card[0]) = [minBot,vars.length]
               D(card[1]) = [0,Imax[1]]
               ...
               D(card[cards.length-1]) = [0,Imax[cards.length-1]]
     In conjunction with a set of arithmetic constraints :
      card[cards.length-1] + ... + card[3] + card[2] + card[1] <= Imax[1]
      card[cards.length-1] + ... + card[3] + card[2] <= Imax[2]
      card[cards.length-1] + ... + card[3] <= Imax[3]
      ...
      card[cards.length-1] + card[cards.length-2] <= Imax[cards.length-2]
    
    
    
    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1325 5779b9a9-2447-0410-887e-bb98d57c6de4

---
+++ choco-contribs/src/main/java/choco/cp/solver/constraints/global/ordered/OrderedGccManager.java
@@ -0,0 +1,35 @@
+package choco.cp.solver.constraints.global.ordered;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thierry.petit(a)emn.fr
+ * Date: 9 nov. 2009
+ * Time: 17:59:00
+ *
+ */
+
+import choco.cp.model.managers.IntConstraintManager;
+import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.Solver;
+import choco.kernel.solver.constraints.SConstraint;
+import choco.kernel.solver.variables.integer.IntDomainVar;
+
+import java.util.HashSet;
+
+public class OrderedGccManager extends IntConstraintManager {
+
+	public SConstraint makeConstraint(Solver solver,
+            						  IntegerVariable[] variables,
+            						  Object parameters,
+            						  HashSet<String> options){
+		IntDomainVar[] vars = new IntDomainVar[variables.length];
+		for(int i=0; i<vars.length; i++) {
+			vars[i] = solver.getVar(variables[i]);
+		}
+		Object[] params = (Object[]) parameters;
+		int[] Imax = (int[]) params[1];
+		int minBot = (Integer) params[2];
+		return new OrderedGcc(vars,Imax,minBot);
+	}
+
+}
---
+++ choco-contribs/src/main/java/choco/cp/solver/constraints/global/ordered/OrderedGccTest.java
@@ -0,0 +1,268 @@
+package choco.cp.solver.constraints.global.ordered;
+
+import static choco.Choco.makeIntVar;
+import choco.cp.model.CPModel;
+import choco.cp.solver.CPSolver;
+import choco.kernel.common.logging.ChocoLogging;
+import choco.kernel.model.Model;
+import choco.kernel.model.constraints.ComponentConstraint;
+import choco.kernel.model.variables.integer.IntegerVariable;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: thierry.petit(a)emn.fr
+ * Date: 9 nov. 2009
+ * Time: 18:03:32
+ * Test of OrderedGcc
+ */
+public class OrderedGccTest {
+
+    protected final static Logger LOGGER = ChocoLogging.getTestLogger();
+    public static boolean debug = false;
+
+    @Test
+    public void testIsSatisfiedTrue() {
+        Model m = new CPModel();
+		IntegerVariable[] vars = new IntegerVariable[4];
+		vars[0] = makeIntVar("var0 ", 1, 5);
+		vars[1] = makeIntVar("var1 ", 2, 5);
+		vars[2] = makeIntVar("var2 ", 0, 3);
+		vars[3] = makeIntVar("var3 ", 1, 5);
+        int minValue = 0;
+		int maxValue = 5;
+		int[] Imax1 = new int[maxValue-minValue+1];
+		Imax1[0] = vars.length;
+		Imax1[1] = 3;
+		Imax1[2] = 2;
+		Imax1[3] = 2;
+		Imax1[4] = 1;
+		Imax1[5] = 0;
+        int minBot = 1;
+	    Object[] p = new Object[3];
+	    p[0] = vars;
+	    p[1] = Imax1;
+	    p[2] = minBot;
+	    ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+        m.addConstraint(c);
+        CPSolver s = new CPSolver();
+        s.read(m);
+        assertTrue(s.getCstr(c).isSatisfied());
+    }
+
+    @Test
+    public void testIsSatisfiedFalse() {
+        Model m = new CPModel();
+		IntegerVariable[] vars = new IntegerVariable[4];
+		vars[0] = makeIntVar("var0 ", 1, 5);
+		vars[1] = makeIntVar("var1 ", 2, 5);
+		vars[2] = makeIntVar("var2 ", 0, 3);
+		vars[3] = makeIntVar("var3 ", 1, 5);
+        int minValue = 0;
+		int maxValue = 5;
+		int[] Imax1 = new int[maxValue-minValue+1];
+		Imax1[0] = vars.length;
+		Imax1[1] = 3;
+		Imax1[2] = 2;
+		Imax1[3] = 2;
+		Imax1[4] = 1;
+		Imax1[5] = 0;
+        int minBot = 2;
+	    Object[] p = new Object[3];
+	    p[0] = vars;
+	    p[1] = Imax1;
+	    p[2] = minBot;
+	    ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+        m.addConstraint(c);
+        CPSolver s = new CPSolver();
+        s.read(m);
+        assertFalse(s.getCstr(c).isSatisfied());
+  }
+
+     @Test
+    public void testSolution1() {
+        Model m = new CPModel();
+		IntegerVariable[] vars = new IntegerVariable[4];
+		vars[0] = makeIntVar("var0 ", 1, 5);
+		vars[1] = makeIntVar("var1 ", 2, 5);
+		vars[2] = makeIntVar("var2 ", 0, 3);
+		vars[3] = makeIntVar("var3 ", 1, 5);
+        int minValue = 0;
+		int maxValue = 5;
+		int[] Imax1 = new int[maxValue-minValue+1];
+		Imax1[0] = vars.length;
+		Imax1[1] = 3;
+		Imax1[2] = 1;
+		Imax1[3] = 0;
+		Imax1[4] = 0;
+		Imax1[5] = 0;
+        int minBot = 1;
+	    Object[] p = new Object[3];
+	    p[0] = vars;
+	    p[1] = Imax1;
+	    p[2] = minBot;
+	    ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+        m.addConstraint(c);
+        CPSolver s = new CPSolver();
+        s.read(m);
+        s.solve();
+        // minBot = 1 => at least one 0
+        assertTrue(s.getVar(vars[2]).getVal()==0);
+        // Imax[2] = 1 => var0 == 1 && var3 == 1
+        assertTrue((s.getVar(vars[0]).getVal()==1) && (s.getVar(vars[3]).getVal()==1));
+        // Imax[3] = 0 => var1 == 2
+        assertTrue(s.getVar(vars[1]).getVal()==2);
+    }
+
+     @Test
+    public void testNumberOfSolutions1() {
+        Model m = new CPModel();
+		IntegerVariable[] vars = new IntegerVariable[4];
+		vars[0] = makeIntVar("var0 ", 1, 5);
+		vars[1] = makeIntVar("var1 ", 2, 5);
+		vars[2] = makeIntVar("var2 ", 0, 3);
+		vars[3] = makeIntVar("var3 ", 1, 5);
+        int minValue = 0;
+		int maxValue = 5;
+		int[] Imax1 = new int[maxValue-minValue+1];
+		Imax1[0] = vars.length;
+		Imax1[1] = 3;
+		Imax1[2] = 1;
+		Imax1[3] = 1;
+		Imax1[4] = 0;
+		Imax1[5] = 0;
+        int minBot = 1;
+        // nombre de solutions = 2
+        // 1 2 0 1, 1 3 0 1
+	    Object[] p = new Object[3];
+	    p[0] = vars;
+	    p[1] = Imax1;
+	    p[2] = minBot;
+	    ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+        m.addConstraint(c);
+        CPSolver s = new CPSolver();
+        s.read(m);
+        s.solveAll();
+        assertTrue(s.getNbSolutions()==2);
+    }
+
+    @Test
+       public void testNumberOfSolutions2() {
+           Model m = new CPModel();
+           IntegerVariable[] vars = new IntegerVariable[4];
+           vars[0] = makeIntVar("var0 ", -1, 3);
+           vars[1] = makeIntVar("var1 ", 0, 3);
+           vars[2] = makeIntVar("var2 ", -2, 1);
+           vars[3] = makeIntVar("var3 ", -1, 3);
+           int minValue = -2;
+           int maxValue = 3;
+           int[] Imax1 = new int[maxValue-minValue+1];
+           Imax1[0] = vars.length;
+           Imax1[1] = 3;
+           Imax1[2] = 1;
+           Imax1[3] = 1;
+           Imax1[4] = 0;
+           Imax1[5] = 0;
+           int minBot = 1;
+           // nombre de solutions = 2
+           // -1 0 -2 -1, -1 1 -2 -1
+           Object[] p = new Object[3];
+           p[0] = vars;
+           p[1] = Imax1;
+           p[2] = minBot;
+           ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+           m.addConstraint(c);
+           CPSolver s = new CPSolver();
+           s.read(m);
+           s.solveAll();
+           assertTrue(s.getNbSolutions()==2);
+       }
+
+    @Test
+        public void testNumberOfSolutions3() {
+            Model m = new CPModel();
+            IntegerVariable[] vars = new IntegerVariable[4];
+            vars[0] = makeIntVar("var0 ", 1, 2);
+            vars[1] = makeIntVar("var1 ", 1, 5);
+            vars[2] = makeIntVar("var2 ", 0, 3);
+            vars[3] = makeIntVar("var3 ", 1, 5);
+            int minValue = 0;
+            int maxValue = 5;
+            int[] Imax1 = new int[maxValue-minValue+1];
+            Imax1[0] = vars.length;
+            Imax1[1] = 3;
+            Imax1[2] = 1;
+            Imax1[3] = 1;
+            Imax1[4] = 0;
+            Imax1[5] = 0;
+            int minBot = 1;
+            // nombre de solutions = 6
+            // 1 1 0 1, 2 1 0 1, 1 2 0 1, 1 1 0 2,
+            // 1 3 0 1, 1 1 0 3,
+            Object[] p = new Object[3];
+            p[0] = vars;
+            p[1] = Imax1;
+            p[2] = minBot;
+            ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+            m.addConstraint(c);
+            CPSolver s = new CPSolver();
+            s.read(m);
+            s.solve();
+            if(debug) {
+                System.out.println(s.pretty());
+            }
+            while(s.nextSolution()){
+                  if(debug) {
+                      System.out.println(s.pretty());
+                  }
+            }
+            assertTrue(s.getNbSolutions()==6);
+        }
+
+     @Test
+        public void testNumberOfSolutions4() {
+            Model m = new CPModel();
+            IntegerVariable[] vars = new IntegerVariable[4];
+            vars[0] = makeIntVar("var0 ", 0, 1);
+            vars[1] = makeIntVar("var1 ", 0, 4);
+            vars[2] = makeIntVar("var2 ", -1, 2);
+            vars[3] = makeIntVar("var3 ", 0, 4);
+            int minValue = -1;
+            int maxValue = 4;
+            int[] Imax1 = new int[maxValue-minValue+1];
+            Imax1[0] = vars.length;
+            Imax1[1] = 3;
+            Imax1[2] = 1;
+            Imax1[3] = 1;
+            Imax1[4] = 0;
+            Imax1[5] = 0;
+            int minBot = 1;
+            // nombre de solutions = 6
+            // 0 0 -1 0, 1 0 -1 0, 0 1 -1 0, 0 0 -1 1,
+            // 0 2 -1 0, 0 0 -1 2,
+            Object[] p = new Object[3];
+            p[0] = vars;
+            p[1] = Imax1;
+            p[2] = minBot;
+            ComponentConstraint c = new ComponentConstraint(OrderedGccManager.class,(Object) p,vars);
+            m.addConstraint(c);
+            CPSolver s = new CPSolver();
+            s.read(m);
+            s.solve();
+            if(debug) {
+                System.out.println(s.pretty());
+            }
+            while(s.nextSolution()){
+                 if(debug) {
+                      System.out.println(s.pretty());
+                 }
+            }
+            assertTrue(s.getNbSolutions()==6);
+        }
+
+
+}
