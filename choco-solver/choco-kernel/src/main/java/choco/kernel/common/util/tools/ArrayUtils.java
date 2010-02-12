7b6ac071b716b4de18070e55715067496516a22b
menana <menana@5779b9a9-2447-0410-887e-bb98d57c6de4>
2010-02-12 13:22:14 +0000

    add flattenSubMatrix method in ArrayUtils for Sophie
    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1446 5779b9a9-2447-0410-887e-bb98d57c6de4

--- choco-solver/choco-kernel/src/main/java/choco/kernel/common/util/tools/ArrayUtils.java
+++ choco-solver/choco-kernel/src/main/java/choco/kernel/common/util/tools/ArrayUtils.java
@@ -36,278 +36,286 @@ import java.util.*;
  * Update : Choco 2.1.0
  */
 public final class ArrayUtils {
-	private final static TLongHashSet HASHSET = new TLongHashSet();
-	private final static ArrayList<Object> LIST = new ArrayList<Object>();
+    private final static TLongHashSet HASHSET = new TLongHashSet();
+    private final static ArrayList<Object> LIST = new ArrayList<Object>();
 
 
-	private ArrayUtils() {
-		super();
-	}
+    private ArrayUtils() {
+        super();
+    }
 
-	public static int[] zeroToN(int n) {
-		final int[] r = new int[n];
-		for (int i = 0; i < n; i++) {
-			r[i] = i;
-		}
-		return r;
-	}
+    public static int[] zeroToN(int n) {
+        final int[] r = new int[n];
+        for (int i = 0; i < n; i++) {
+            r[i] = i;
+        }
+        return r;
+    }
 
-	public static int[] oneToN(int n) {
-		final int[] r = new int[n];
-		for (int i = 1; i <= n; i++) {
-			r[i - 1] = i;
-		}
-		return r;
-	}
+    public static int[] oneToN(int n) {
+        final int[] r = new int[n];
+        for (int i = 1; i <= n; i++) {
+            r[i - 1] = i;
+        }
+        return r;
+    }
 
-	public static int[] linspace(int begin, int end) {
-		if(end > begin) {
-			int[] r = new int[end-begin];
-			for (int i = begin; i < end; i++) {
-				r[ i -begin] = i;
-			}
-			return r;
-		}else return null;
-	}
+    public static int[] linspace(int begin, int end) {
+        if(end > begin) {
+            int[] r = new int[end-begin];
+            for (int i = begin; i < end; i++) {
+                r[ i -begin] = i;
+            }
+            return r;
+        }else return null;
+    }
 
-	@SuppressWarnings("unchecked")
-	public static <T> T[] getColumn(final T[][] array, final int column) {
-		if (array != null && array.length > 0
-				&& column >= 0 && array[0].length > column) {
-			T[] res = (T[]) java.lang.reflect.Array.newInstance(array[0][column].getClass(), array.length);
-			for (int i = 0; i < array.length; i++) {
-				res[i] = array[i][column];
-			}
-			return res;
-		}
-		return null;
-	}
+    @SuppressWarnings("unchecked")
+    public static <T> T[] getColumn(final T[][] array, final int column) {
+        if (array != null && array.length > 0
+                && column >= 0 && array[0].length > column) {
+            T[] res = (T[]) java.lang.reflect.Array.newInstance(array[0][column].getClass(), array.length);
+            for (int i = 0; i < array.length; i++) {
+                res[i] = array[i][column];
+            }
+            return res;
+        }
+        return null;
+    }
 
-	public static <T> int length(final T[]... arrays) {
-		int length = 0;
-		for (T[] array : arrays) {
-			if (array != null) length += array.length;
-		}
-		return length;
-	}
+    public static <T> int length(final T[]... arrays) {
+        int length = 0;
+        for (T[] array : arrays) {
+            if (array != null) length += array.length;
+        }
+        return length;
+    }
 
-	public static <T> T get(int index, final T[]... arrays) {
-		int shift = index;
-		for (T[] tab : arrays) {
-			if (shift < tab.length) {
-				return tab[shift];
-			} else {
-				shift -= tab.length;
-			}
-		}
-		return null;
-	}
+    public static <T> T get(int index, final T[]... arrays) {
+        int shift = index;
+        for (T[] tab : arrays) {
+            if (shift < tab.length) {
+                return tab[shift];
+            } else {
+                shift -= tab.length;
+            }
+        }
+        return null;
+    }
 
-	public static <T> T get(int index, final List<T>... arrays) {
-		int shift = index;
-		for (List<T> tab : arrays) {
-			if (shift < tab.size()) {
-				return tab.get(shift);
-			} else {
-				shift -= tab.size();
-			}
-		}
-		return null;
-	}
+    public static <T> T get(int index, final List<T>... arrays) {
+        int shift = index;
+        for (List<T> tab : arrays) {
+            if (shift < tab.size()) {
+                return tab.get(shift);
+            } else {
+                shift -= tab.size();
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Append two Arrays
-	 *
-	 * @param toAppend array of arrays to append
-	 * @return a new Array composed of both given in parameters.
-	 */
-	 @SuppressWarnings("unchecked")
-	 public static <T> T[] append(T[]... toAppend) {
-		 int total = length(toAppend);
-		 T[] ret = (T[]) java.lang.reflect.Array.newInstance(toAppend.getClass().getComponentType().getComponentType(), total);
-		 int pos = 0;
-		 for (T[] tab : toAppend) {
-			 if (tab != null) {
-				 System.arraycopy(tab, 0, ret, pos, tab.length);
-				 pos += tab.length;
-			 }
-		 }
-		 return ret;
-	 }
+    /**
+     * Append two Arrays
+     *
+     * @param toAppend array of arrays to append
+     * @return a new Array composed of both given in parameters.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] append(T[]... toAppend) {
+        int total = length(toAppend);
+        T[] ret = (T[]) java.lang.reflect.Array.newInstance(toAppend.getClass().getComponentType().getComponentType(), total);
+        int pos = 0;
+        for (T[] tab : toAppend) {
+            if (tab != null) {
+                System.arraycopy(tab, 0, ret, pos, tab.length);
+                pos += tab.length;
+            }
+        }
+        return ret;
+    }
 
-	 /**
-	  * Reverse all signs of the a given int table.
-	  *
-	  * @param tab array to inverse
-	  */
-	 public static void inverseSign(int[] tab) {
-		 for (int i = 0; i < tab.length; i++) {
-			 tab[i] = -tab[i];
-		 }
-	 }
+    /**
+     * Reverse all signs of the a given int table.
+     *
+     * @param tab array to inverse
+     */
+    public static void inverseSign(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = -tab[i];
+        }
+    }
 
-	 public static void reverse(int[] tab) {
-		 int tmp;
-		 final int n = tab.length;
-		 for (int i = 0; i < n / 2; i++) {
-			 tmp = tab[i];
-			 tab[i] = tab[n - i - 1];
-			 tab[n - i - 1] = tmp;
-		 }
-	 }
+    public static void reverse(int[] tab) {
+        int tmp;
+        final int n = tab.length;
+        for (int i = 0; i < n / 2; i++) {
+            tmp = tab[i];
+            tab[i] = tab[n - i - 1];
+            tab[n - i - 1] = tmp;
+        }
+    }
 
-	 public static <T> void reverse(T[] tab) {
-		 T tmp;
-		 final int n = tab.length-1;
-		 for (int i = 0; i < n / 2; i++) {
-			 tmp = tab[i];
-			 tab[i] = tab[n - i];
-			 tab[n - i] = tmp;
-		 }
-	 }
+    public static <T> void reverse(T[] tab) {
+        T tmp;
+        final int n = tab.length-1;
+        for (int i = 0; i < n / 2; i++) {
+            tmp = tab[i];
+            tab[i] = tab[n - i];
+            tab[n - i] = tmp;
+        }
+    }
 
-	 /**
-	  * apply a permuation on an array
-	  */
-	  @SuppressWarnings("unchecked")
-	  public static <T> void permutation(int[] permutation, T[] tab) {
-		  T[] tmp = (T[]) java.lang.reflect.Array.newInstance(tab[0].getClass(), tab.length);
-		  System.arraycopy(tab, 0, tmp, 0, tab.length);
-		  for (int i = 0; i < tab.length; i++) {
-			  tab[i] = tmp[permutation[i]];
-		  }
-	  }
+    /**
+     * apply a permuation on an array
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> void permutation(int[] permutation, T[] tab) {
+        T[] tmp = (T[]) java.lang.reflect.Array.newInstance(tab[0].getClass(), tab.length);
+        System.arraycopy(tab, 0, tmp, 0, tab.length);
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = tmp[permutation[i]];
+        }
+    }
 
-	  public static <T> List<T> toList(T[] array) {
-		  return Arrays.asList(array);
-	  }
+    public static <T> List<T> toList(T[] array) {
+        return Arrays.asList(array);
+    }
 
-	  public static <T> T[] toArray(Class c, List<T> list) {
-		  //        T[] array = (T[])Array.newInstance(c, list.size());
-		  //        return list.toArray(array);
-		  return list.toArray((T[]) java.lang.reflect.Array.newInstance(c, list.size()));
-	  }
+    public static <T> T[] toArray(Class c, List<T> list) {
+        //        T[] array = (T[])Array.newInstance(c, list.size());
+        //        return list.toArray(array);
+        return list.toArray((T[]) java.lang.reflect.Array.newInstance(c, list.size()));
+    }
 
-	  public static <T> T[] toArray(ArrayList<T> list) {
-		  return toArray(list.get(0).getClass(), list);
-	  }
+    public static <T> T[] toArray(ArrayList<T> list) {
+        return toArray(list.get(0).getClass(), list);
+    }
 
-	  public static <T> T[][] transpose(T[][] matrix) {
-		  T[][] ret = (T[][]) java.lang.reflect.Array.newInstance(matrix.getClass().getComponentType(), matrix[0].length);
-		  for (int i = 0; i < ret.length; i++) {
-			  ret[i] = (T[]) java.lang.reflect.Array.newInstance(matrix[0].getClass().getComponentType(), matrix.length);
-		  }
+    public static <T> T[][] transpose(T[][] matrix) {
+        T[][] ret = (T[][]) java.lang.reflect.Array.newInstance(matrix.getClass().getComponentType(), matrix[0].length);
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = (T[]) java.lang.reflect.Array.newInstance(matrix[0].getClass().getComponentType(), matrix.length);
+        }
 
-		  for (int i = 0; i < matrix.length; i++)
-			  for (int j = 0; j < matrix[i].length; j++)
-				  ret[j][i] = matrix[i][j];
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = 0; j < matrix[i].length; j++)
+                ret[j][i] = matrix[i][j];
 
-		  return ret;
+        return ret;
 
-	  }
+    }
 
-	  public static <T> T[] flatten(T[][] matrix) {
-		  int sz = 0;
-		  for (T[] t : matrix) sz += t.length;
-		  T[] ret = (T[]) java.lang.reflect.Array.newInstance(matrix[0].getClass().getComponentType(), sz);
-		  int k = 0;
-		  for (T[] ta : matrix) {
-			  for (T t : ta)
-				  ret[k++] = t;
-		  }
-		  return ret;
-	  }
-	  
-	  public static int[] flatten(int[][] matrix) {
-		  int sz = 0;
-		  for (int[] t : matrix) sz += t.length;
-		  final int[] ret = new int[sz];
-		  int k = 0;
-		  for (int[] ta : matrix) {
-			  for (int t : ta)
-				  ret[k++] = t;
-		  }
-		  return ret;
-	  }
+    public static <T> T[] flatten(T[][] matrix) {
+        int sz = 0;
+        for (T[] t : matrix) sz += t.length;
+        T[] ret = (T[]) java.lang.reflect.Array.newInstance(matrix[0].getClass().getComponentType(), sz);
+        int k = 0;
+        for (T[] ta : matrix) {
+            for (T t : ta)
+                ret[k++] = t;
+        }
+        return ret;
+    }
 
-	  /**
-	   * create a new array which contains sorted distinct values;
-	   *
-	   * @param values
-	   * @return
-	   */
-	  public static int[] getNonRedundantSortedValues(int[] values) {
-		  return createNonRedundantSortedValues(new TIntArrayList(values));
-	  }
+    public static <T> T[] flattenSubMatrix(int iMin, int iLength, int jMin, int jLength, T[][] matrix)
+    {
+        T[] ret = (T[]) java.lang.reflect.Array.newInstance(matrix[0].getClass().getComponentType(), iLength*jLength);
+        for (int i = 0, k=0; i < iLength; i++, k += jLength)
+            System.arraycopy(matrix[iMin+i], jMin, ret, k, jLength);
+        return ret;
+    }
 
-	  /**
-	   * create an array which contains sorted distinct values. do not modify the original list
-	   */
-	  public static int[] getNonRedundantSortedValues(TIntArrayList values) {
-		  return createNonRedundantSortedValues((TIntArrayList) values.clone());
-	  }
+    public static int[] flatten(int[][] matrix) {
+        int sz = 0;
+        for (int[] t : matrix) sz += t.length;
+        final int[] ret = new int[sz];
+        int k = 0;
+        for (int[] ta : matrix) {
+            for (int t : ta)
+                ret[k++] = t;
+        }
+        return ret;
+    }
 
-	  public static int[] createNonRedundantSortedValues(TIntArrayList values) {
-		  values.sort();
-		  int offset = 1;
-		  while (offset < values.size()) {
-			  while (values.get(offset - 1) == values.get(offset)) {
-				  values.remove(offset);
-				  if (offset == values.size()) {
-					  break;
-				  }
-			  }
-			  offset++;
-		  }
-		  return values.toNativeArray();
-	  }
+    /**
+     * create a new array which contains sorted distinct values;
+     *
+     * @param values
+     * @return
+     */
+    public static int[] getNonRedundantSortedValues(int[] values) {
+        return createNonRedundantSortedValues(new TIntArrayList(values));
+    }
 
-	  @SuppressWarnings({"unchecked"})
-	  public static <V extends IIndex> V[] getNonRedundantObjects(V[] all) {
-		  HASHSET.clear();
-		  LIST.clear();
-		  for (V v : all) {
-			  if (!HASHSET.contains(v.getIndex())) {
-				  LIST.add(v);
-				  HASHSET.add(v.getIndex());
-			  }
-		  }
-		  if (LIST.size() != all.length) {
-			  V[] a = (V[]) java.lang.reflect.Array.newInstance((Class<? extends V[]>)all.getClass().getComponentType(), LIST.size());
-			  LIST.toArray(a);
-			  return a;
-		  }
-		  return all;
-	  }
+    /**
+     * create an array which contains sorted distinct values. do not modify the original list
+     */
+    public static int[] getNonRedundantSortedValues(TIntArrayList values) {
+        return createNonRedundantSortedValues((TIntArrayList) values.clone());
+    }
+
+    public static int[] createNonRedundantSortedValues(TIntArrayList values) {
+        values.sort();
+        int offset = 1;
+        while (offset < values.size()) {
+            while (values.get(offset - 1) == values.get(offset)) {
+                values.remove(offset);
+                if (offset == values.size()) {
+                    break;
+                }
+            }
+            offset++;
+        }
+        return values.toNativeArray();
+    }
 
     @SuppressWarnings({"unchecked"})
-	  public static <V extends IIndex> V[] getNonRedundantObjects(Class classe, V[] all) {
-		  HASHSET.clear();
-		  LIST.clear();
-		  for (V v : all) {
-			  if (!HASHSET.contains(v.getIndex())) {
-				  LIST.add(v);
-				  HASHSET.add(v.getIndex());
-			  }
-		  }
-		  if (LIST.size() != all.length) {
-			  V[] a = (V[]) java.lang.reflect.Array.newInstance(classe, LIST.size());
-			  LIST.toArray(a);
-			  return a;
-		  }
-		  return all;
-	  }
+    public static <V extends IIndex> V[] getNonRedundantObjects(V[] all) {
+        HASHSET.clear();
+        LIST.clear();
+        for (V v : all) {
+            if (!HASHSET.contains(v.getIndex())) {
+                LIST.add(v);
+                HASHSET.add(v.getIndex());
+            }
+        }
+        if (LIST.size() != all.length) {
+            V[] a = (V[]) java.lang.reflect.Array.newInstance((Class<? extends V[]>)all.getClass().getComponentType(), LIST.size());
+            LIST.toArray(a);
+            return a;
+        }
+        return all;
+    }
 
-	  @SuppressWarnings("unchecked")
-	  public static <T extends Comparable<T>> T[] sort(Set<T> set) {
-		  final LinkedList<T> tmpl = new LinkedList<T>(set);
-		  if (tmpl.isEmpty()) {
-			  return null;
-		  } else {
-			  T[] tmpa = (T[]) java.lang.reflect.Array.newInstance(tmpl.getFirst().getClass(), tmpl.size());
-			  tmpl.toArray(tmpa);
-			  Arrays.sort(tmpa);
-			  return tmpa;
-		  }
-	  }
+    @SuppressWarnings({"unchecked"})
+    public static <V extends IIndex> V[] getNonRedundantObjects(Class classe, V[] all) {
+        HASHSET.clear();
+        LIST.clear();
+        for (V v : all) {
+            if (!HASHSET.contains(v.getIndex())) {
+                LIST.add(v);
+                HASHSET.add(v.getIndex());
+            }
+        }
+        if (LIST.size() != all.length) {
+            V[] a = (V[]) java.lang.reflect.Array.newInstance(classe, LIST.size());
+            LIST.toArray(a);
+            return a;
+        }
+        return all;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Comparable<T>> T[] sort(Set<T> set) {
+        final LinkedList<T> tmpl = new LinkedList<T>(set);
+        if (tmpl.isEmpty()) {
+            return null;
+        } else {
+            T[] tmpa = (T[]) java.lang.reflect.Array.newInstance(tmpl.getFirst().getClass(), tmpl.size());
+            tmpl.toArray(tmpa);
+            Arrays.sort(tmpa);
+            return tmpa;
+        }
+    }
 }
