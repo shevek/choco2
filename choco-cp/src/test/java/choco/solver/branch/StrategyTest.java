6846a3f07ce15ed3294242e812219bece15e0a32
cprudhom <cprudhom@5779b9a9-2447-0410-887e-bb98d57c6de4>
2012-05-03 15:23:54 +0000

    few things before release
    git-svn-id: svn://svn.code.sf.net/p/choco/code/trunk@1903 5779b9a9-2447-0410-887e-bb98d57c6de4

--- choco-cp/src/test/java/choco/solver/branch/StrategyTest.java
+++ choco-cp/src/test/java/choco/solver/branch/StrategyTest.java
@@ -31,6 +31,8 @@ import choco.Choco;
 import choco.Options;
 import choco.cp.model.CPModel;
 import choco.cp.solver.CPSolver;
+import choco.cp.solver.preprocessor.PreProcessCPSolver;
+import choco.cp.solver.preprocessor.PreProcessConfiguration;
 import choco.cp.solver.search.integer.branching.AssignVar;
 import choco.cp.solver.search.integer.valiterator.IncreasingDomain;
 import choco.cp.solver.search.integer.valselector.MinVal;
@@ -44,6 +46,8 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static choco.Choco.*;
+
 /**
  * User:    charles
  * Date:    9 sept. 2008
@@ -51,13 +55,13 @@ import org.junit.Test;
 public class StrategyTest {
 
 
-    static class IncorrectVarSelector extends AbstractIntVarSelector{
+    static class IncorrectVarSelector extends AbstractIntVarSelector {
         IncorrectVarSelector(IntDomainVar[] vars) {
             super(null, vars);
         }
 
         public IntDomainVar selectVar() {
-            if(!vars[0].isInstantiated()){
+            if (!vars[0].isInstantiated()) {
                 return vars[0];
             }
             return null;
@@ -66,7 +70,7 @@ public class StrategyTest {
 
     @Test
     @Ignore
-    public void badSelectors(){
+    public void badSelectors() {
         Model m = new CPModel();
         Solver s = new CPSolver();
         IntegerVariable v1 = Choco.makeIntVar("v1", 0, 2, Options.V_ENUM);
@@ -75,22 +79,53 @@ public class StrategyTest {
         s.read(m);
         s.setVarIntSelector(new IncorrectVarSelector(s.getVar(v1, v2)));
         s.setValIntSelector(new MinVal());
-        try{
+        try {
             s.solve();
             Assert.fail("Every variables have been instantiated");
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
 
     @Test
-    public void testB2986005(){
+    public void testB2986005() {
         CPSolver s = new CPSolver();
         IntDomainVar v = s.createEnumIntVar("v", 1, 2);
         s.attachGoal(new AssignVar(new StaticVarOrder(s, new IntDomainVar[]{v}), new IncreasingDomain()));
         s.solveAll();
         // one ROOT node  + 1 node (v=1) + 1 node (v=2)
         Assert.assertEquals("incorrect nb of nodes", 3, s.getNodeCount());
+    }
+
+    @Test
+    public void testB2986006() {
+        CPModel m = new CPModel();
+        int n = 99;
+        IntegerVariable[] vars = new IntegerVariable[n];
+        for (int i = 0; i < n - 1; i++) {
+            vars[i] = Choco.makeIntVar("v" + i, 0, n, Options.V_BOUND);
+        }
+        vars[n - 1] = Choco.makeIntVar("v" + (n - 1), 0, 1, Options.V_BOUND);
+        for (int i = 0; i < n - 1; i++) {
+            m.addConstraint(Choco.eq(vars[i], vars[i + 1]));
+        }
+//        m.addConstraint(Choco.allDifferent(Options.C_ALLDIFFERENT_BC, vars));
+        CPSolver s = new CPSolver();
+        s.read(m);
+        s.solve();
+    }
+
+    @Test
+    public void testB29860067() {
+        IntegerVariable v1 = makeIntVar("v", 0, Choco.MAX_UPPER_BOUND, Options.V_BOUND);
+        IntegerVariable v2 = makeIntVar("abs(v-c)", 0, MAX_UPPER_BOUND, Options.V_BOUND);
+        CPModel mod = new CPModel();
+        mod.addConstraint(eq(v2, abs(minus(v1, 10))));
+        mod.setDefaultExpressionDecomposition(true);
+        Solver s = new PreProcessCPSolver();
+        PreProcessConfiguration.cancelPreProcess(s.getConfiguration());
+        s.read(mod);
+        System.out.println(s.pretty());
     }
 
 
